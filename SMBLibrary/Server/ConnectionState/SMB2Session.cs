eccf02f841a14afcabaed9ebb2781114570a79bc
胡正 <zheng@sharecreators.com>
2025-04-26 00:32:17 +0800

    fix: 优化持久化ID 的设置

--- SMBLibrary/Server/ConnectionState/SMB2Session.cs
+++ SMBLibrary/Server/ConnectionState/SMB2Session.cs
@@ -145,7 +145,10 @@ namespace SMBLibrary.Server
                     fileID.Volatile = volatileFileID.Value;
                     // [MS-SMB2] FileId.Persistent MUST be set to Open.DurableFileId.
                     // Note: We don't support durable handles so we use volatileFileID.
-                    fileID.Persistent = volatileFileID.Value;
+                    fileID.Persistent = CacheHelper.TryGet<ulong>($"fileID_{shareName}/{relativePath}", () =>
+                    {
+                        return volatileFileID.Value;
+                    }, 5);
                     m_openFiles.Add(volatileFileID.Value, new OpenFileObject(treeID, shareName, relativePath, handle, fileAccess));
                     return fileID;
                 }
