8b62469e821e1a4275f73c007ce9437108a6bf45
胡正 <zheng@sharecreators.com>
2025-05-04 10:23:00 +0800

    fix： 精简日志

--- SMBLibrary/Server/SMB2/ReadWriteResponseHelper.cs
+++ SMBLibrary/Server/SMB2/ReadWriteResponseHelper.cs
@@ -28,7 +28,7 @@ namespace SMBLibrary.Server.SMB2
             {
                 if (!((FileSystemShare)share).HasReadAccess(session.SecurityContext, openFile.Path))
                 {
-                    state.LogToServer(Severity.Verbose, "Read from '{0}{1}' failed. User '{2}' was denied access.", share.Name, openFile.Path, session.UserName);
+                    //state.LogToServer(Severity.Verbose, "Read from '{0}{1}' failed. User '{2}' was denied access.", share.Name, openFile.Path, session.UserName);
                     return new ErrorResponse(request.CommandName, NTStatus.STATUS_ACCESS_DENIED);
                 }
             }
