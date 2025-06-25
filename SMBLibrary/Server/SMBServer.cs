04ce9e16c6343ff21f8fe05a2b4b6d9a8436cc59
胡正 <zheng@sharecreators.com>
2025-06-25 15:07:19 +0800

    fix: 切换为TAP模式

--- SMBLibrary/Server/SMBServer.cs
+++ SMBLibrary/Server/SMBServer.cs
@@ -220,8 +220,8 @@ namespace SMBLibrary.Server
                     // So in either case (NetBios over TCP or Direct TCP Transport) we will receive an NBT packet.
                     //clientSocket.BeginReceive(state.ReceiveBuffer.Buffer, state.ReceiveBuffer.WriteOffset, state.ReceiveBuffer.AvailableLength, 0, ReceiveCallback, state);
 
-                    //TAPReceiveMsg receiveMsg = new TAPReceiveMsg(this);
-                    IOCPReceiveMsg receiveMsg = new IOCPReceiveMsg(this);
+                    TAPReceiveMsg receiveMsg = new TAPReceiveMsg(this);
+                    //IOCPReceiveMsg receiveMsg = new IOCPReceiveMsg(this);
                     receiveMsg.InitAndStartReceive(state);
                 }
                 catch (ObjectDisposedException)
